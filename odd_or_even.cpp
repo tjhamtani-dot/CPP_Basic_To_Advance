246e2baef0bd2babb88d9cf2518c31e2fd6e5777
Tanisha Jhamtani <tjhamtani@gmail.com>
2026-07-06 13:46:24 +0530

    odd_or_even.cpp

---
+++ odd_or_even.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter a number ";
+    cin>>a;
+
+    if(a%2==0)
+    { cout<<"even number "<<a; }
+    else
+    { cout<<"odd number "<<a; }
+
+    return 0;
+}
