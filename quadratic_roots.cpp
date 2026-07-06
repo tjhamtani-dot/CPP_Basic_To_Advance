3bfb916d96a53e66400b2f952fb47eea64b0a0d2
Tanisha Jhamtani <tjhamtani@gmail.com>
2026-07-06 16:47:37 +0530

    Updated quadratic_roots.cpp

--- quadratic_roots.cpp
+++ quadratic_roots.cpp
@@ -3,16 +3,30 @@
 using namespace std;
 int main()
 {
-    int a, b, c;
-    float root1, root2;
+    float a, b, c;
+    float d, root1, root2;         
     cout<<"enter value of a, b, c";
 
     cin>>a>>b>>c;
+    
+    d = b*b - 4*a*c;
 
-    root1= ( -b + sqrt(b*b - 4*a*c))/(2*a);
-    root2= ( -b + sqrt(b*b - 4*a*c))/(2*a);
+    if(d==0)
+    {
+        root1= -b/(2*a);
+        cout<<"roots of this quadratic equation is real and equal "<<root1;
+    }
+    else if(d>0)
+    {
+        root1= (-b + sqrt(d))/(2*a);
+        root2= (-b + sqrt(d))/(2*a);
+        cout<<"roots of this quadratic equation is real and unequal "<<root1<<root2;
+    }
+    else
+    {
+        cout<<"roots of this quadratic equation is imaginary ";
+    }
 
-    cout<<"roots of this quadratic equation is "<<root1<<root2;
     return 0;
     
 }
